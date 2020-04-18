c5a3165f1520cefb759f01d57a1f24514340f428
Mitchell Schmidt <MitchellT.S@outlook.com>
2020-04-17 19:43:08 -0500

    added testing and GET/POST structure

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@
 
 require('dotenv').config();
 const express = require('express');
+
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const ArticlesService = require('./articles-service');
+const articlesRouter = require('./articles-router');
 
 const app = express();
 
@@ -19,17 +20,15 @@ app.use(helmet());
 
 // body
 
-app.get('/articles', (req, res, next) => {
-	const knexInstance = req.app.get('db');
-	ArticlesService.getAllArticles(knexInstance)
-		.then((articles) => {
-			res.json(articles);
-		})
-		.catch(next);
-});
+app.use('/articles', articlesRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello, boilerplate!');
+});
+
+app.get('/xss', (req, res) => {
+	res.cookie('secretToken', '1234567890');
+	res.sendFile(__dirname + '/xss-example.html');
 });
 
 // error handling
