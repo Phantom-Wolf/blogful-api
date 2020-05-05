952cfef054ed8ee707b02e252593105d4c987dfe
Mitchell Schmidt <mitchellt.s@outlook.com>
2020-05-04 19:18:29 -0500

    added realationships to database tables

--- src/app.js
+++ src/app.js
@@ -1,45 +1,54 @@
 // imports
 
-require('dotenv').config();
-const express = require('express');
-
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
-const articlesRouter = require('./articles-router');
-
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const articlesRouter = require("./articles-router");
+const usersRouter = require("./users/users-router");
+const commentsRouter = require("./comments/comments-router");
 const app = express();
 
 // middleware
 
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
 // body
 
-app.use('/articles', articlesRouter);
+app.use("/api/articles", articlesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/comments", commentsRouter);
 
-app.get('/', (req, res) => {
-	res.send('Hello, boilerplate!');
+app.get("/", (req, res) => {
+	res.send("Hello, boilerplate!");
 });
 
-app.get('/xss', (req, res) => {
-	res.cookie('secretToken', '1234567890');
-	res.sendFile(__dirname + '/xss-example.html');
+app.get("/xss", (req, res) => {
+	res.cookie("secretToken", "1234567890");
+	res.sendFile(__dirname + "/xss-example.html");
 });
 
 // error handling
 
 app.use(function errorHandler(error, req, res, next) {
 	let response;
-	if (NODE_ENV === 'production') {
-		response = { error: { message: 'server error' } };
+	if (NODE_ENV === "production") {
+		response = {
+			error: {
+				message: "server error",
+			},
+		};
 	} else {
 		console.log(error);
-		response = { message: error.message, error };
+		response = {
+			message: error.message,
+			error,
+		};
 	}
 	res.status(500).json(response);
 });
--- src/articles-service.js
+++ src/articles-service.js
@@ -1,31 +1,24 @@
 const ArticlesService = {
-	getAllArticles(knex) {
-		return knex.select('*').from('blogful_articles');
-	},
 	insertArticle(knex, newArticle) {
 		return knex
 			.insert(newArticle)
-			.into('blogful_articles')
-			.returning('*')
+			.into("blogful_articles")
+			.returning("*")
 			.then((rows) => {
 				return rows[0];
 			});
 	},
+	getAllArticles(knex) {
+		return knex.select("*").from("blogful_articles");
+	},
 	getById(knex, id) {
-		return knex
-			.from('blogful_articles')
-			.select('*')
-			.where('id', id)
-			.first();
+		return knex.from("blogful_articles").select("*").where("id", id).first();
 	},
 	deleteArticle(knex, id) {
-		return knex.from('blogful_articles').where({ id }).delete();
+		return knex.from("blogful_articles").where({ id }).delete();
 	},
 	updateArticle(knex, id, newArticleFields) {
-		return knex
-			.from('blogful_articles')
-			.where({ id })
-			.update(newArticleFields);
+		return knex.from("blogful_articles").where({ id }).update(newArticleFields);
 	},
 };
 
